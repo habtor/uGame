c6efa742f44d45ecf243dcf2dabb1cfcb5e3dc23
Saleh <39927806+habtor@users.noreply.github.com>
2024-04-23 20:54:50 +0200

    added newgames section

--- src/components/main/Games.jsx
+++ src/components/main/Games.jsx
@@ -1,9 +1,11 @@
 import Category from "../main/Categories";
 import HotGames from "./HotGames";
+import NewGame from "./NewGame";
 
 function Games() {
   return (
     <>
+      <NewGame />
       <HotGames />
       <Category />
     </>
